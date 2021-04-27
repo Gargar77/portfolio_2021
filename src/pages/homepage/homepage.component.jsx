06e49cdfb8d732442aa4e1d6272bbcd4b4f5a513
gargar77 <gbautista2@knights.ucf.edu>
2021-04-27 18:09:32 -0400

    added buttons to homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,14 +4,22 @@ import './homepage.styles.scss';
 import Particles from 'react-particles-js';
 import {PARTICLE_PARAMS} from '../../particles';
 import Header from '../../components/Header/header.component';
+import CustomButton from '../../components/custom-button/custom-button.component';
 
 class HomePage extends React.Component {
    
     render() {
         return (
             <div className="homepage">
-               <Particles style={{filter:'blur(2px)'}} width={document.clientWidth} height={window.innerHeight - 4} params={PARTICLE_PARAMS}/>
-               <Header withSubheading titleSize={6} subtitleSize={4} klass="homepage-header" title="Gary Bautista" subtitle="Software Engineer"/>
+               <Particles style={{filter:'blur(3px)'}} width={document.clientWidth} height={window.innerHeight - 4} params={PARTICLE_PARAMS}/>
+               <main className="homepage-content">
+                <Header withSubheading titleSize={6} subtitleSize={4} klass="homepage-header" title="Gary Bautista" subtitle="Software Engineer"/>
+                <div className="homepage-button-container">
+                    <CustomButton buttonType="about">about</CustomButton>
+                    <CustomButton buttonType="resume">resume</CustomButton>
+                    <CustomButton buttonType="projects">projects</CustomButton>
+                </div>
+               </main>
             </div>
         );
     }
