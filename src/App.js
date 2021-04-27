00101ef31f5b533459edaba73df1f5bce8a426f1
gargar77 <gbautista2@knights.ucf.edu>
2021-04-27 15:38:28 -0400

    added homepage as route

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { Switch,Route } from 'react-router-dom';
 
 import './App.css';
 
+import Homepage from './pages/homepage/homepage.component';
+
 class App extends React.Component {
   render() {
     return(
       <div className="App">
+        <Switch>
+          <Route exact path='/' component={Homepage}/>
+        </Switch>
       </div>
     );
   }
