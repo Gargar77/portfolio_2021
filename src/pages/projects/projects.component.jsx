16e2769294755892dfd30631ac7c6d32769d14a8
gargar77 <gbautista2@knights.ucf.edu>
2021-04-28 14:17:14 -0400

    added project data to website

--- src/pages/projects/projects.component.jsx
+++ src/pages/projects/projects.component.jsx
@@ -6,9 +6,12 @@ import Nav from '../../components/Nav/nav.component';
 import NavItem from '../../components/Nav/nav-item/nav-item.component';
 import ProjectList from '../../components/project-list/project-list.component';
 
+import Particles from 'react-particles-js';
+import { PARTICLE_PARAMS } from '../../particles';
 
 const Projects = (props) => (
     <div className="projects">
+        <Particles style={{filter:'blur(3px)',position:'absolute'}} width={document.clientWidth} height={window.innerHeight - 4} params={PARTICLE_PARAMS}/>
            <Nav>
             <NavItem redirect click={props.animate} path="/">Home</NavItem>
             <NavItem redirect click={props.animate} path="about">about</NavItem>
@@ -19,3 +22,4 @@ const Projects = (props) => (
 );
 
 export default Projects;
+
--- src/projectData.js
+++ src/projectData.js
@@ -1,39 +1,32 @@
 export const PROJECT_DATA = [
     {
-        title:'sample',
-        projectUrl:'www.google.com',
-        imageUrl:'https://kidscreen.com/wp/wp-content/uploads/2019/10/barney.jpg',
-        description:'blah blah blah blah blah'
+        title:'frontend CRM',
+        projectUrl:'#',
+        imageUrl:'https://firebasestorage.googleapis.com/v0/b/portfolio-284cf.appspot.com/o/admin_portal_mobile.png?alt=media&token=d2963161-2611-4e48-a783-a249648f2eb1',
+        description:'Developed a frontend CRM UI for a nonprofit whose mission is to fund orphanages across the globe'
     },
     {
-        title:'sample',
-        projectUrl:'www.google.com',
-        imageUrl:'https://kidscreen.com/wp/wp-content/uploads/2019/10/barney.jpg',
-        description:'blah blah blah blah blah'
+        title:'Aid2many',
+        projectUrl:'https://aid2many.org/Faqs',
+        imageUrl:'https://firebasestorage.googleapis.com/v0/b/portfolio-284cf.appspot.com/o/aid2many_faq_page_mobile.png?alt=media&token=b406b5f9-a130-4a31-94d6-5430ba8f30a9',
+        description:"Redesigned the entire frontend of this non-profit's website for a more modern aesthethic and UX flow"
     },
     {
-        title:'sample',
-        projectUrl:'www.google.com',
-        imageUrl:'https://kidscreen.com/wp/wp-content/uploads/2019/10/barney.jpg',
-        description:'blah blah blah blah blah'
+        title:'CleanTrac',
+        projectUrl:'https://github.com/Gargar77/CleanTrac',
+        imageUrl:'https://firebasestorage.googleapis.com/v0/b/portfolio-284cf.appspot.com/o/auth_mobile.png?alt=media&token=484ac295-38f6-4f00-8354-6d5fc7ba8a25',
+        description:"Redesigned the entire frontend of this non-profit's website for a more modern aesthethic and UX flow"
     },
     {
-        title:'sample',
-        projectUrl:'www.google.com',
-        imageUrl:'https://kidscreen.com/wp/wp-content/uploads/2019/10/barney.jpg',
-        description:'blah blah blah blah blah'
+        title:'meteors',
+        projectUrl:'https://github.com/Gargar77/Meteors',
+        imageUrl:'https://firebasestorage.googleapis.com/v0/b/portfolio-284cf.appspot.com/o/meteors.png?alt=media&token=00fa6577-3a75-440a-b820-8df7aff053a5',
+        description:"Redesigned the entire frontend of this non-profit's website for a more modern aesthethic and UX flow"
     },
     {
-        title:'sample',
-        projectUrl:'www.google.com',
-        imageUrl:'https://kidscreen.com/wp/wp-content/uploads/2019/10/barney.jpg',
-        description:'blah blah blah blah blah'
+        title:'Neon Tic',
+        projectUrl:'https://github.com/Gargar77/neon-tic-tac-toe',
+        imageUrl:'https://firebasestorage.googleapis.com/v0/b/portfolio-284cf.appspot.com/o/neon_tic_winner.PNG?alt=media&token=06bef875-f274-46e8-9b2a-52e8a6236a03',
+        description:"Redesigned the entire frontend of this non-profit's website for a more modern aesthethic and UX flow"
     },
-    {
-        title:'sample',
-        projectUrl:'www.google.com',
-        imageUrl:'https://kidscreen.com/wp/wp-content/uploads/2019/10/barney.jpg',
-        description:'blah blah blah blah blah'
-    }
-    
 ]
