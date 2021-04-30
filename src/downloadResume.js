ee7175e22017387e25891bde16e6eacae3761e1a
gargar77 <gbautista2@knights.ucf.edu>
2021-04-30 12:00:23 -0400

    updated resume link

--- src/downloadResume.js
+++ src/downloadResume.js
@@ -1,4 +1,4 @@
-const RESUME_LINK = "https://firebasestorage.googleapis.com/v0/b/portfolio-284cf.appspot.com/o/Resume_web.pdf?alt=media&token=06fa4051-3c93-41a1-9635-5bed702b0cef";
+const RESUME_LINK = "https://firebasestorage.googleapis.com/v0/b/portfolio-284cf.appspot.com/o/Resume_web.pdf?alt=media&token=fd7e8ede-77c9-42cc-b92a-6b5e111254a6";
 
 export const downloadResume = () => (
     window.open(
